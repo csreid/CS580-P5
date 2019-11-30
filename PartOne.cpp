24926b008149fa6299855a7bac7f2b17aa5715aa
Kaley J. Rittichier <kaleykoo@gmail.com>
2019-11-30 09:34:33 -0500

    This is my version of Part One
    I’m sorry; I didn’t see that you had already started Part 1. I had just bookmarked the Project list and saw that it was not in “In Progress” yet so I did it. I uploaded just because I figured I might as well. It seems to meet the criteria, but we can use whichever.

---
+++ PartOne.cpp
@@ -0,0 +1,44 @@
+#include "utility.h"
+//Team 6: Cameron Reid and Kaley Rittichier
+
+void makeFile(string file_name,  int items[]) {
+	ofstream file;
+	file.open(file_name);
+	if (file.is_open()) {
+		for (int i = 0; i < 128; i++) {
+			if(items[static_cast <int>(static_cast <char> (i))] > 0){
+				char c = static_cast <char> (i);
+				int freq = items[static_cast <int>(c)];
+				if (static_cast <int>(i) == 10) {
+					file << "LF" << " " << freq << endl;
+				}
+				else {
+					file << c << " "<< freq << endl;
+				}			
+			}
+		}
+	}
+	else cout << "Unable to open/create file";
+}
+
+int main() {
+	ifstream in_stream;
+	in_stream.open("clear.txt");
+	if (in_stream.fail()) {
+		cout << "Unable to find input file.";
+		exit(1);
+	}
+	char c = NULL;
+	int freq = 0;
+	int items[128];
+	for (int i = 0; i < 128; i++) {
+		items[i] = 0;
+	}
+	while (!in_stream.eof()) {//I'm not reading "the entire file into memory", correct? 
+		in_stream.get(c);
+		items[static_cast <int>(c)]++;
+	}
+	in_stream.close();
+	makeFile("freq.txt", items);
+	return 0;
+}
---
+++ utility.h
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef UTILITY_H
+#define UTILITY_H
+
+//Gives ANSI version of standard includes
+
+#include <iostream>
+#include <iomanip> 
+#include <limits>
+#include <cmath>
+#include <cstdlib>
+#include <cstddef>
+#include <fstream>
+#include <cctype>
+#include <ctime>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <stdio.h> 
+
+using namespace std;
+
+#endif //UTILITY_H
+#pragma once
+#pragma once
