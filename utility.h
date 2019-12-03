20c7cc11d604f579f5ce5750cea4241de8b4f450
Kaley J. Rittichier <kaleykoo@gmail.com>
2019-12-03 11:35:18 -0500

    Working on simple test data I found.
    It should be tested with Part2's data once that code is completed. I named test data the same as actual data so we should just change the files.

---
+++ utility.h
@@ -0,0 +1,28 @@
+#pragma once
+#ifndef UTILITY_H
+#define UTILITY_H
+
+//Gives ANSI version of standard includes
+#include <queue> 
+#include <iostream>
+#include <sstream>
+#include <iomanip> 
+#include <limits>
+#include <cmath>
+#include <cstdlib>
+#include <cstddef>
+#include <fstream>
+#include <cctype>
+#include <ctime>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <stdio.h> 
+#include <map> 
+
+using namespace std;
+
+#endif //UTILITY_H
+#pragma once
+#pragma once
+
